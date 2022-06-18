6cc1e365f750615f60039befb76a8ddf290940a5
ninngiorgadze <ninngiorgadze@gmail.com>
2022-06-18 22:45:01 +0400

    Minor

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -10,7 +10,7 @@ const Auth = () => {
     }
 
     return (
-        <form className="bg-white shadow-md width rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit(onSubmit)}>
+        <form className="bg-white shadow-md auth-form rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit(onSubmit)}>
             <div className="mb-4">
                 <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
                     Username
